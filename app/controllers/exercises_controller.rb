2fd6aa5876d888456149c32b4bba7829d7458d13
joshuadelapenha <joshuadelapenha@yahoo.com>
2020-09-24 13:19:02 -0400

    clicking exercises links to logs

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,4 +2,22 @@ class ExercisesController < ApplicationController
     def index
         @exercises = Exercise.all
     end 
+
+    def show
+        @exercise = Exercise.find(params[:id])
+    end
+
+    def new
+        @exercise = Exercise.new
+    end
+    
+
+    def create
+        @exercise = Exercise.new
+        @exercise.name = params[:name]
+        @exercise.category = params[:category]
+        @exercise.save
+        redirect_to exercise_path(@exerciselog)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get "/lifters", to: "lifters#index"
-  # get "/exercises", to: "exercises#index"
-  # get "/exerciselogs", to: "exerciselogs#index"
+  
+  get "/exercises", to: "exercises#index"
+  get "/exercises/:id", to: "exercises#show"
+  get "/exerciselogs", to: "exerciselogs#index"
+  get "/exerciselogs/:id", to: "exerciselogs#show"
 
-  resources :exerciselogs, only: [:index, :show, :new, :create]
+  # resources :exerciselogs, only: [:index, :show, :new, :create]
 
 
 end
